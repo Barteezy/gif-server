88d7e089b8a8b7e7e88688abe4a0af3f3e05a662
Luke Bartel <bartelluke@gmail.com>
2015-02-02 17:23:16 -0700

    rails app gif-server we made in class

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.2.0'
+gem 'sqlite3'
+gem 'jbuilder', '~> 2.0'
+gem 'rack-cors', :require => 'rack/cors'
+gem 'active_model_serializers', '~> 0.9.1'
+gem 'sdoc', '~> 0.4.0', group: :doc
---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Gif.create!(gifUrl: "http://s3-ec.buzzfed.com/static/2014-07/18/8/enhanced/webdr09/anigif_enhanced-buzz-32533-1405685455-8.gif", tag: "hedgehog")
+Gif.create!(gifUrl: "http://s3-ec.buzzfed.com/static/2014-07/18/8/enhanced/webdr02/anigif_enhanced-buzz-21069-1405685596-7.gif", tag: "llama")
